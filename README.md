0f3b1045ca0f425dc5f05e6294c6f97b279c160f
Tonia-SE <64790742+Tonia-SE@users.noreply.github.com>
2021-12-12 11:33:02 +0300

    add: add npm packajes, gitignore, first test

--- README.md
+++
@@ -1,3 +0,0 @@
-# unit-test-course
-
-follow udemy course https://www.udemy.com/course/unit-testing-typescript-nodejs/
---
+++ src/tests/Utils.test.ts
@@ -0,0 +1,5 @@
+describe('Utils test suit', () => {
+  it('first test', () => {
+    console.log('tests work');
+  });
+});
